0fb0579b8e6b21f7f8969f803883ca053dfee585
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-08-20 15:22:39 -0300

    conditions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Evento from './components/Event';
 import Form from './components/Form'
+import Condicional from './components/Condicional';
 
 function App() {
 	
 return(
 		<div className="App">
-			<Evento />
-			<Form />
+			<h1>Flamengo</h1>
+			<Condicional />
 		</div>
 	);
 }
