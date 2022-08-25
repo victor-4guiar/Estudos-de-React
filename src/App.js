b48aa56867f28b90ee41913a77d2748e524ecdbe
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-08-25 20:39:22 -0300

    MiPaisxd

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {useState} from 'react';
 import SeuNome from './components/SeuNome';
 import Saudacao from './components/Saudacao'
 import Perfil from './components/Perfil';
+import MiPais from './components/MiPais/MiPais';
 
 function App() {
 	const [nome, setName] = useState();
 	
 	return(
 		<div className="App">
-			<Perfil pfp="https://i0.wp.com/esqrever.com/wp-content/uploads/2018/10/bolsonaro-lgbti-brasil-homofobia-policc81tica-presidente-rainbow-arco-icc81ris-2018.png?resize=750%2C629&ssl=1" nome="Victor Cobain" idade="17"/>
+			<MiPais flag="https://cdn.mises.org.br/images/articles_thumbs/2270.png" nombre="Ascua" descripcion="Republica Social Democrata del continente Rojo como Ascua" lema="Orden y Progreso"/>
+			<MiPais flag="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/255px-Flag_of_Uruguay.svg.png" nombre="Cuuuuuuuuuuu" descripcion="República social de Cu" lema="Cu de Apito"/>
 		</div>
 	);
 }
---
+++ src/components/MiPais/MiPais.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function MiPais({flag, nombre, descripcion, lema}){
+	return(
+		<div id="main">
+			<img src={flag} height="230px" width="400px" />
+			<h1>{nombre} ({lema})</h1>
+			<h2>{descripcion}</h2>
+		</div>
+	)
+}
+
+export default MiPais;
